348834899199110c9eca786e692a801ef73ece03
aman123956 <you@example.com>
2021-06-13 12:51:03 +0530

    Added social icons

--- src/components/Footer/Social.js
+++ src/components/Footer/Social.js
@@ -1,6 +1,15 @@
 import styles from './Social.module.css'
+import ReactDOM from 'react-dom'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook,faTwitter,faLinkedin,faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+
+
+
 
 function Social() {
+   
     return (
         <div className={styles.socialBox}>
             <h2 className={styles.heading1}>
@@ -8,7 +17,11 @@ function Social() {
             </h2>
             <div className={styles.dashedLine}></div>
             <div className={styles.socialIconCorner}>
-                
+               
+                <FontAwesomeIcon  className={styles.socialIcon} icon={faFacebook} />
+                <FontAwesomeIcon  className={styles.socialIcon} icon={faLinkedin} />
+                <FontAwesomeIcon  className={styles.socialIcon} icon={faTwitter} />
+                <FontAwesomeIcon  className={styles.socialIcon} icon={faInstagram} />
             </div>
             
         </div>
